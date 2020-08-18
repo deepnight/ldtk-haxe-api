f31ca679ece08b0cb163edf5f6013ea613410cbe
Sébastien Bénard <github@deepnight.net>
2020-08-18 11:21:23 +0200

    IntGrid parsing (neko stack overflow)

--- src/LedParser.hx
+++ src/LedParser.hx
@@ -6,6 +6,7 @@ import haxe.macro.Expr;
 class LedParser {
 	public macro static function parse(filePath:String) {
 		// Read file
+		timer("read");
 		var raw =
 			try sys.io.File.read(filePath, false).readAll().toString()
 			catch(e:Dynamic) {
@@ -13,12 +14,15 @@ class LedParser {
 				return null;
 			}
 
+		timer("json");
 		var json : Dynamic =
 			try haxe.Json.parse(raw)
 			catch(e:Dynamic) {
 				error("Couldn't parse JSON "+filePath);
 				return null;
 			}
+
+		timer("macro");
 
 		var p = Context.currentPos();
 
@@ -50,12 +54,62 @@ class LedParser {
 			var layersJson : Array<Dynamic> = cast json.layerInstances;
 			var layers : Array<{ id:String, e:Expr }> = [];
 			for(json in layersJson) {
-				trace(json.__identifier);
+				var cWid : Int = json.__cWid;
+				var cHei : Int = json.__cHei;
+				// trace(json.__identifier+" : "+json.__type);
 				var layerFields = makeObjectFields(json, [
 					"pxOffsetX",
 					"pxOffsetY",
 				]);
 
+				// Misc fields
+				layerFields.push({ field: "cWid", expr: macro $v{cWid} });
+
+				switch( json.__type ) {
+					case "IntGrid":
+						// startTimer();
+						// Build IntGrid layer
+						var intGrid : Array<Dynamic> = json.intGrid;
+						var grid : Array<Array<Int>> = [];
+						for(cx in 0...cWid) {
+							grid[cx] = [];
+							for(cy in 0...cHei)
+								grid[cx][cy] = -1;
+						}
+						for(ig in intGrid) {
+							var cx = coordIdToX( ig.coordId, cWid );
+							var cy = coordIdToY( ig.coordId, cWid );
+							grid[cx][cy] = ig.v;
+						}
+
+						// Build grid expr
+						var lines : Array<Expr> = [];
+						for( cx in 0...cWid) {
+							var cols = [];
+							for( cy in 0...cHei) {
+								// trace(cx);
+								cols.push( macro $v{grid[cx][cy]} );
+							}
+							// trace(cols);
+							lines.push({
+								expr: EArrayDecl(cols),
+								pos: p,
+							});
+							// lines.push( macro $a{cols} );
+						}
+						layerFields.push({
+							field: "intGrid",
+							expr: {
+								expr: EArrayDecl(lines),
+								pos:p,
+							},
+						});
+						// endTimer();
+
+
+					case _:
+						// warning('Unsupported layer type ${json.__type} ("${json.__identifier}")');
+				}
 				// TODO add layer content
 
 				layers.push({
@@ -82,7 +136,13 @@ class LedParser {
 		});
 
 
+		// var test = [ [ 5 ] ];
+		// var e = macro $v{test};
+		// trace(e.expr);
+
+
 		var out = { expr: EObjectDecl(projectFields), pos:p }
+		timer();
 		return out;
 	}
 
@@ -133,6 +193,27 @@ class LedParser {
 
 	static inline function parseColor(hex:String) : String {
 		return "0x"+hex.substr(1,999);
+	}
+
+	static inline function coordIdToX(coordId:Int, cWid:Int) {
+		return coordId - Std.int( coordId / cWid ) * cWid;
+	}
+
+	static inline function coordIdToY(coordId:Int, cWid:Int) {
+		return Std.int( coordId / cWid );
+	}
+
+
+	// Debug timer
+	static var _t = -1.;
+	static var _timerName = "";
+	static inline function timer(?name="") {
+		#if debug
+		if( _t>=0 )
+			trace( Std.int( ( haxe.Timer.stamp()-_t ) * 1000 ) / 1000  + "s " + _timerName );
+		_timerName = name;
+		_t = haxe.Timer.stamp();
+		#end
 	}
 	#end
 }
--- src/Main.hx
+++ src/Main.hx
@@ -5,7 +5,14 @@ class Main {
 		var p = LedParser.parse("../led/app/userFiles/mini.json");
 		trace( p.levels );
 		trace( p.levels_arr[0].identifier );
-		trace( p.levels.Level0.layers.Entities );
+		// trace( p.levels.Level0.layers.Entities );
+		var l = p.levels_arr[0];
+		// trace(l.layers.IntGrid.intGrid[0][0]);
+		// var layer = l.layers.IntGrid;
+		// trace(layer.cWid);
+		// trace(layer.intGrid);
+		// layer.
+		// trace(l.layers.IntGrid.intGrid);
 		// trace(p.levelsAcc.Tutorial_intro.pxWid);
 	}
 }
