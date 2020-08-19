30954e8cce842e08dd690c1f506accedbf4437ef
Sébastien Bénard <github@deepnight.net>
2020-08-19 15:35:18 +0200

    Tests

--- src/Main.hx
+++ src/Main.hx
@@ -3,10 +3,14 @@ class Main {
 
 	public function new() {
 		var p = new test.IntGrid();
+		var l = p.levels.Level0.l_Colls;
+		for(cx in 0...5)
+			if( l.has(cx,0) )
+				trace( dn.Color.intToHex( l.getColor(cx,0) ) );
 
-		var p = new test.Mini();
+		// var p = new test.Mini();
 
-		var p = new test.Gmtk();
+		// var p = new test.Gmtk();
 
 	}
 }
