454965bae079f79fa104afdd82733fc068fef768
Sébastien Bénard <github@deepnight.net>
2020-08-24 13:41:25 +0200

    Added layer type enum

--- src/led/Layer.hx
+++ src/led/Layer.hx
@@ -1,8 +1,15 @@
 package led;
 
+enum LayerType {
+	IntGrid;
+	Tiles;
+	Entities;
+	Unknown;
+}
+
 class Layer {
 	public var identifier : String;
-	public var type : String; // TODO enum
+	public var type : LayerType;
 	public var cWid : Int;
 	public var cHei : Int;
 	public var pxOffsetX: Int;
@@ -10,7 +17,10 @@ class Layer {
 
 	public function new(json:led.JsonTypes.LayerInstJson) {
 		identifier = json.__identifier;
-		type = json.__type;
+		type =
+			try LayerType.createByName(json.__type)
+			catch(e:Dynamic) Unknown;
+		// type = json.__type;
 		cWid = json.__cWid;
 		cHei = json.__cHei;
 		pxOffsetX = json.pxOffsetX;
@@ -24,6 +34,7 @@ class Layer {
 	public inline function isCoordValid(cx,cy) {
 		return cx>=0 && cx<cWid && cy>=0 && cy<cHei;
 	}
+
 
 	inline function getCx(coordId:Int) {
 		return coordId - Std.int(coordId/cWid)*cWid;
