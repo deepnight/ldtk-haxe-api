cc9ea60632bb64c06a7c57fe6a3982d51b605560
Sébastien Bénard <github@deepnight.net>
2020-09-03 12:43:48 +0200

    Moved Assert to libs Better assert methods

--- tests/src/test/Data.hx
+++ tests/src/test/Data.hx
@@ -1,16 +1,17 @@
 package test;
 
+import dn.CiAssert;
+
 class Data {
 	var project : data.GameTest;
 
 	public function new() {
 		project = new data.GameTest();
 
-		var v = true;
-		Assert.check( v==true );
-		Assert.check( project.all_levels!=null );
-		Assert.check( project.all_levels.Level0!=null );
-		Assert.check( project.resolveLevel("Level0")!=null );
+		CiAssert.isNotNull( project );
+		CiAssert.isNotNull( project.all_levels );
+		CiAssert.isNotNull( project.all_levels.Level0 );
+		CiAssert.isNotNull( project.resolveLevel("Level0") );
 	}
 }
 
