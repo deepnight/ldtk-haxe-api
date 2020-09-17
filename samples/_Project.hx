9bdf339657797abcf59f9375550de569c5f92a7c
Sébastien Bénard <github@deepnight.net>
2020-09-17 10:40:16 +0200

    Added some comment

--- samples/_Project.hx
+++ samples/_Project.hx
@@ -1,3 +1,8 @@
+/**
+	This file will contain all the classes and types generated
+	from the "sample.json" LEd project
+**/
+
 private typedef _Tmp = haxe.macro.MacroType<[
 	led.Project.build("res/sample.json")
 ]>;
