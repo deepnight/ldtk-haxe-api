e1c3a4a7023723975dc8489c67d4f57136ca4571
Sébastien Bénard <github@deepnight.net>
2020-11-27 16:24:08 +0100

    Updated world sample & map

--- samples/HeapsRender_World.hx
+++ samples/HeapsRender_World.hx
@@ -22,20 +22,24 @@ class HeapsRender_World extends hxd.App {
 		// Load atlas h2d.Tile from the Heaps resources (could be loaded in other ways)
 		var cavernasAtlasTile = hxd.Res.Cavernas_by_Adam_Saltsman.toTile();
 
+		// Create a single wrapper for all levels
+		var worldWrapper = new h2d.Object( s2d );
+		worldWrapper.scale(0.66); // scale it down so we can fit it on screen
+
 		for( level in project.levels ) {
 			// This wrapper will contain all layers
-			var wrapper = new h2d.Object( s2d );
-			wrapper.x = level.worldX;
-			wrapper.y = level.worldY;
+			var levelWrapper = new h2d.Object( worldWrapper );
+			levelWrapper.x = level.worldX;
+			levelWrapper.y = level.worldY;
 
 			// Pure auto-layer (background walls)
-			level.l_Background.render(cavernasAtlasTile, wrapper);
+			level.l_Background.render(cavernasAtlasTile, levelWrapper);
 
 			// IntGrid Auto-layer (walls, ladders, etc.)
-			level.l_Collisions.render(cavernasAtlasTile, wrapper);
+			level.l_Collisions.render(cavernasAtlasTile, levelWrapper);
 
 			// Tiles layer (manually added details)
-			level.l_Custom_tiles.render(cavernasAtlasTile, wrapper);
+			level.l_Custom_tiles.render(cavernasAtlasTile, levelWrapper);
 		}
 
 	}
