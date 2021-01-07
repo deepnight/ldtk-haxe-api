b3ac8d822a8d4b41e2cb0512caac77b8e485c306
Sébastien Bénard <github@deepnight.net>
2021-01-07 13:09:25 +0100

    Minor doc fix

--- src/ldtk/Json.hx
+++ src/ldtk/Json.hx
@@ -313,9 +313,9 @@ typedef FieldInstanceJson = {
 
 
 /**
-	If you're writing your own LDtk importer, you should probably ignore MOST stuff in the `defs` section, as it contains data that are specifically useful to the editor. Data that is useful to game devs is duplicated in fields prefixed with a double underscore (eg. `__identifier` or `__type`).
+If you're writing your own LDtk importer, you should probably ignore MOST stuff in the `defs` section, as it contains data that are specifically useful to the editor. Data that is useful to game devs is duplicated in fields prefixed with a double underscore (eg. `__identifier` or `__type`).
 
-	The 2 only definition types you might need here are **Tilesets** and **Enums**.
+The 2 only definition types you might need here are **Tilesets** and **Enums**.
 **/
 @section("2")
 @display("Definitions")
