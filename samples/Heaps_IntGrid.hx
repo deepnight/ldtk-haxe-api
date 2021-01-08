e5dd5c9042b4320d2a70149d3e7c8cc1600ccfb1
Sébastien Bénard <github@deepnight.net>
2021-01-08 10:47:42 +0100

    Cleanup

--- samples/Heaps_IntGrid.hx
+++ samples/Heaps_IntGrid.hx
@@ -22,20 +22,22 @@ class Heaps_IntGrid extends hxd.App {
 		// Layer data
 		var layer = project.all_levels.West.l_Collisions;
 
-		// Render background
+		// Prepare a h2d.Graphics to render layer
 		var g = new h2d.Graphics(s2d);
+
+		// Render background
 		g.beginFill(project.bgColor_int);
 		g.drawRect(0, 0, layer.cWid*layer.gridSize, layer.cHei*layer.gridSize);
 		g.endFill();
 
-		// Display IntGrid layer
+		// Render IntGrid layer cells
 		for(cx in 0...layer.cWid)
 		for(cy in 0...layer.cHei) {
-			if( !layer.hasValue(cx,cy) )
+			if( !layer.hasValue(cx,cy) ) // skip empty cells
 				continue;
 
-			var c = layer.getColorInt(cx,cy);
-			g.beginFill(c);
+			var color = layer.getColorInt(cx,cy);
+			g.beginFill(color);
 			g.drawRect(cx*layer.gridSize, cy*layer.gridSize, layer.gridSize, layer.gridSize);
 		}
 	}
