45c368ef4bdfc4f532beffc720e672a126151334
Sebastien Benard <github@deepnight.net>
2022-01-24 13:53:19 +0100

    Lab

--- tests/lab/src/Main.hx
+++ tests/lab/src/Main.hx
@@ -3,13 +3,34 @@ import LdtkProject;
 
 class Main extends dn.Process {
 	var bmp : h2d.Bitmap;
-	var lp : LdtkProject;
+	var p : LdtkProject;
+	var level : LdtkProject_Level;
 
 	public function new() {
 		super();
+
 		createRoot(Boot.ME.s2d);
-		
-		lp = new LdtkProject();
+
+		p = new LdtkProject();
+		level = p.all_levels.Level_0;
+
+		// Render
+		root.setScale( dn.heaps.Scaler.bestFit_i(level.pxWid, level.pxHei) );
+		root.addChild( level.l_Collisions.render() );
+
+		// Level tile field
+		var t = level.f_levelTile_getTile();
+		if( t!=null )
+			new h2d.Bitmap(t, root);
+
+		// Entity tiles
+		for(e in level.l_Entities.all_TilesTest) {
+			trace(e);
+			trace(" skin="+e.f_skin_infos);
+			trace(" opt="+e.f_optTile_infos);
+			var bmp = new h2d.Bitmap(e.getTile(), root);
+			bmp.setPosition( e.pixelX, e.pixelY );
+		}
 	}
 
 	override function onResize() {
@@ -18,6 +39,5 @@ class Main extends dn.Process {
 
 	override function update() {
 		super.update();
-		trace(ftime);
 	}
 }
