02acfab3464d7732e330cf0321edc8c59971fdb6
Sebastien Benard <github@deepnight.net>
2022-01-24 13:53:14 +0100

    Updated test

--- tests/Main.hx
+++ tests/Main.hx
@@ -117,7 +117,7 @@ class Main {
 			CiAssert.isTrue( project.all_levels.Main_tests.l_EntityTest.all_Mob.length!=0 );
 			CiAssert.isTrue( project.all_levels.Main_tests.l_EntityTest.all_Test.length!=0 );
 			CiAssert.equals( project.all_levels.Main_tests.l_EntityTest.all_Unused.length, 0 );
-			CiAssert.isNotNull( project.all_levels.Main_tests.l_EntityTest.all_Mob[0].defaultTileInfos );
+			CiAssert.isNotNull( project.all_levels.Main_tests.l_EntityTest.all_Mob[0].smartTileInfos );
 
 			// Entities
 			var hero = project.all_levels.Main_tests.l_EntityTest.all_Hero[0];
